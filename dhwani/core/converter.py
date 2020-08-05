444e1e490ba930e3b50c41f63018c389ee9ce8d3
Yashas ND <ndyashas@gmail.com>
2020-08-05 23:11:33 +0530

    Fixed documentation bugs in the dhwani.core.converter.Converter class

--- dhwani/core/converter.py
+++ dhwani/core/converter.py
@@ -3,15 +3,15 @@ import dhwani.core.core_utils.converter_utils as converter_utils
 
 
 class Converter:
-    """The Converter class is the main entry point for Dhwani. You can
-    make an object of this class and configure it to perform phonetic conversion
-    from one language to another.
+    """The :class:`dhwani.Converter` (implementation at :class:`dhwani.core.converter.Converter`) 
+    class is the main entry point for Dhwani. You can make an object of this class and configure 
+    it to perform phonetic conversion from one language to another.
 
     :param src_lang_code: ISO 639-3 code of the source language.
     :type src_lang_code: str
     
     :param dest_lang_code: ISO 639-3 code of the destination language.
-    :type src_lang_code: str
+    :type dest_lang_code: str
     
     """
 
@@ -31,11 +31,11 @@ class Converter:
         """Converts the full source text from source language to destination
         language.
 
-        :param src_text: The text in :param:`src_lang_code` language to be converted
+        :param src_text: The text in ``src_lang_code`` language to be converted
         :type src_text: str
 
-        :return: The text in :param:`dest_lang_code` language converted 
-            from :param:`src_lang_code`
+        :return: The text in ``dest_lang_code`` language converted 
+            from ``src_lang_code``
 
         :rtype: str
         """
