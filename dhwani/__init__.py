733f100f759273441dad7952e37ae2ac6b05033a
Yashas ND <ndyashas@gmail.com>
2020-08-20 19:45:20 +0530

    Additional functions exposed in the main module

--- dhwani/__init__.py
+++ dhwani/__init__.py
@@ -1,3 +1,5 @@
 from dhwani.core.converter import Converter
+from dhwani.mappings.utils import print_current_support
+from dhwani.mappings.utils import get_supported_conversions
 
 __version__ = "0.0.3.dev1"
